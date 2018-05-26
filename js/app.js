a1084370db27000c1d1c411e10e1110a24ba220b
WK196 <wangkuang@WKdeMacBook-Pro.local>
2018-05-26 16:23:13 +0800

    commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.post('/reg', function (req, res) {
+	console.log(req.params.mobile);
+  	console.log(req.params.pwd);
+  	console.log(req.query.mobile);
+  	console.log(req.query.pwd);
+  	res.send('Hello World!');
+});
+
+var server = app.listen(3002, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
